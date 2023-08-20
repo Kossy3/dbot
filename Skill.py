44153bd42eb294a247f3b6728d5a0fe5b86a350f
Kossy3 <103818564+Kossy3@users.noreply.github.com>
2023-08-20 16:03:53 +0900

    SP調整 スキル追加

--- Skill.py
+++ Skill.py
@@ -1,7 +1,12 @@
 from Action import *
 from Charactor import *
 
-@Attacks.create("とっしん", 10, "敵にとっしんし、ダメージを与える")
+@Attacks.create("びんた", 2, "小ダメージを与える")
 async def test(self: Attack, to: Charactor, by: Charactor):
   damage = self.calc_damage(to, by, 10)
+  await to.damage(damage)
+
+@Attacks.create("でこぴん", 10, "大ダメージを与える")
+async def test(self: Attack, to: Charactor, by: Charactor):
+  damage = self.calc_damage(to, by, 50)
   await to.damage(damage)
