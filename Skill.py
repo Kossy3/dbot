1a5a995f1ad75ce84083acdc958d1645f0210596
Kossy3 <103818564+Kossy3@users.noreply.github.com>
2023-08-20 11:30:41 +0900

    アノテーション追加

--- Skill.py
+++ Skill.py
@@ -1,6 +1,7 @@
 from Action import *
+from Charactor import *
 
 @Attacks.create("とっしん", 10, "敵にとっしんし、ダメージを与える")
-async def test(self: Attack, to, by):
+async def test(self: Attack, to: Charactor, by: Charactor):
   damage = self.calc_damage(to, by, 10)
   await to.damage(damage)
