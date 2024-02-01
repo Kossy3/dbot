fb74e2629e4bc2b57a0cf888c7d6c024aadb5161
Kossy3 <103818564+Kossy3@users.noreply.github.com>
2024-02-01 18:07:15 +0900

    render環境設定

--- jinro.py
+++ jinro.py
@@ -1,13 +1,15 @@
 class Scene:
-  scenes = "夜", "朝"
+  
   def __init__(self):
-    self.scene = ”夜”
+    self.scene = "夜"
 
   def next(self):
+    scenes = "夜", "朝"
     i = scenes.index(self.scene)
     self.scene = scenes[i+1]
 
-class Job
+class Job:
+  ...
 
 class JinroBot:
   def __init__(self, client):
